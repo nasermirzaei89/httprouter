3a847ead42117076385260a6003ed11dac4693e0
Naser Mirzaei <nasermirzaei89@gmail.com>
2019-06-15 10:39:30 +0430

    add handle, head and options methods

--- httprouter.go
+++ httprouter.go
@@ -12,11 +12,14 @@ type handler struct {
 // Handler interface
 type Handler interface {
 	http.Handler
+	Handle(method, pattern string, handlerFunc http.HandlerFunc)
 	Get(pattern string, handlerFunc http.HandlerFunc)
+	Head(pattern string, handlerFunc http.HandlerFunc)
 	Post(pattern string, handlerFunc http.HandlerFunc)
 	Put(pattern string, handlerFunc http.HandlerFunc)
 	Patch(pattern string, handlerFunc http.HandlerFunc)
 	Delete(pattern string, handlerFunc http.HandlerFunc)
+	Options(pattern string, handlerFunc http.HandlerFunc)
 }
 
 type route struct {
@@ -44,22 +47,34 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.NotFound(w, r)
 }
 
+func (h *handler) Handle(method, pattern string, handlerFunc http.HandlerFunc) {
+	h.routes = append(h.routes, route{method, regexp.MustCompile(pattern), handlerFunc})
+}
+
 func (h *handler) Get(pattern string, handlerFunc http.HandlerFunc) {
-	h.routes = append(h.routes, route{http.MethodGet, regexp.MustCompile(pattern), handlerFunc})
+	h.Handle(http.MethodGet, pattern, handlerFunc)
+}
+
+func (h *handler) Head(pattern string, handlerFunc http.HandlerFunc) {
+	h.Handle(http.MethodHead, pattern, handlerFunc)
 }
 
 func (h *handler) Post(pattern string, handlerFunc http.HandlerFunc) {
-	h.routes = append(h.routes, route{http.MethodPost, regexp.MustCompile(pattern), handlerFunc})
+	h.Handle(http.MethodPost, pattern, handlerFunc)
 }
 
 func (h *handler) Put(pattern string, handlerFunc http.HandlerFunc) {
-	h.routes = append(h.routes, route{http.MethodPut, regexp.MustCompile(pattern), handlerFunc})
+	h.Handle(http.MethodPut, pattern, handlerFunc)
 }
 
 func (h *handler) Patch(pattern string, handlerFunc http.HandlerFunc) {
-	h.routes = append(h.routes, route{http.MethodPatch, regexp.MustCompile(pattern), handlerFunc})
+	h.Handle(http.MethodPatch, pattern, handlerFunc)
 }
 
 func (h *handler) Delete(pattern string, handlerFunc http.HandlerFunc) {
-	h.routes = append(h.routes, route{http.MethodDelete, regexp.MustCompile(pattern), handlerFunc})
+	h.Handle(http.MethodDelete, pattern, handlerFunc)
+}
+
+func (h *handler) Options(pattern string, handlerFunc http.HandlerFunc) {
+	h.Handle(http.MethodOptions, pattern, handlerFunc)
 }
